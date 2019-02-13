165de02976962219fc43180447e83ad597284480
Juan Cortines <Juan.Cortines@pega.com>
2019-02-13 22:17:34 +0100

    closes #11

--- process-tax-records.js
+++ process-tax-records.js
@@ -1,286 +1,106 @@
-/**
- * Released under the MIT license
- */
-
 (() => {
 
-	'use strict'
+    const theYear = 2018
 
-	const PERIOD_START = '2017-01-01'
+    const padNumber = number =>
+        number < 10 ? '0' + number : number
 
-	const PERIOD_END = '2017-31-12'
+    const moveOneDayBack = date => {
+        date.setDate(date.getDate() - 1)
+        return date
+    }
 
-	const COMPANY_ID = '93N'
+    const getCurrencyDateString = date =>
+        date.getFullYear() + '-' + padNumber((date.getMonth() + 1)) + '-' + padNumber(date.getDate())
 
-	const PLAN_ID = '93N'
+    const fetchExchangeRate = date =>
+        fetch(`https://api.nbp.pl/api/exchangerates/rates/a/usd/${getCurrencyDateString(date)}/?format=json`)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json()
+                        .then(data => data.rates[0].mid)
+                }
+                if (response.status === 404) {
+                    return undefined;
+                }
+                return Promise.reject(Error('problem fetching rate from nbp.pl'));
+            })
 
-	const JSON_BASE = '/app-bin/spc/ba/sps'
+    const getExchangeRate = (date, tries = 3) => {
+        if (tries > 0) {
+            return fetchExchangeRate(moveOneDayBack(date))
+                .then(rate => rate || getExchangeRate(date, tries - 1))
+        }
+        return Promise.reject(Error('no rate found!'))
+    }
 
-	const padNumber = number => number < 10 ? '0' + number : number
+    const session = JSON.parse(sessionStorage.getItem('session_state'))
 
-	const getCurrencyDateString = date => date.getFullYear() + '-' + padNumber((date.getMonth() + 1)) + '-' + padNumber(date.getDate())
+    const fetchData = url =>
+        fetch(url, {
+            credentials: 'include',
+            headers: {
+                Authorization: session.api.sessionToken
+            }
+        })
+        .then(response => response.json())
 
-	const getURLDateStringYDM = date => date.getFullYear() + '-' + padNumber(date.getDate()) + '-' + padNumber((date.getMonth() + 1))
+    const fetchSaleDetails = transactionId =>
+        fetchData(session.api.baseUri + session.api.baseLegacyUriSuffix + '/withdrawals/' + transactionId)
 
-	const getURLDateStringYMD = date => date.getFullYear() + '-' + padNumber((date.getMonth() + 1)) + '-' + padNumber(date.getDate())
+    const fetchRtSaleDetails = transactionId =>
+        fetchData(session.api.baseUri + session.api.baseLegacyUriSuffix + '/withdrawals/realtimeTransactions/' + transactionId)
 
-	const parseDollars = dollars => dollars.replace('$', '').replace(/,/g, '')
+    const fetchHistoryRecords = () =>
+        fetchData(session.api.baseUri + session.api.baseLegacyUriSuffix + '/participants/' + session.participant.activeAccount.employeePK + '/txHistory')
+            .then(data => data.txs)
 
-	const fetchData = url =>
-		fetch(url, {
-			credentials: 'include',
-			headers: new Headers({
-				'X-XSRF-TOKEN': getXsrfToken()
-			})
-		})
-		.then(response => response.text())
-		.then(text => JSON.parse(filterResponseGarbage(text)))
+    const processSaleDetails = sale =>
+        fetchExchangeRate(new Date(sale.settlementDate))
+            .then(exchangeRate => {
+                const longTermCostTransactions = (sale.costBasis.longTerm && sale.costBasis.longTerm.rows) || []
+                const shortTermCostTransactions = (sale.costBasis.shortTerm && sale.costBasis.shortTerm.rows) || []
+                return longTermCostTransactions.concat(shortTermCostTransactions).map(transaction => {
+                    let taxableAmountUSD;
+                    const purchaseDate = new Date(transaction.purchaseDate)
+                    const marketValuePerShare = Number(transaction.marketValuePerShare.amount)
+                    const bookValuePerShare = Number(transaction.bookValuePerShare.amount)
+                    const quantity = Number(transaction.quantity)
+                    if (purchaseDate.getFullYear() < 2018) {
+                        taxableAmountUSD = Math.max((marketValuePerShare - bookValuePerShare) * quantity, 0)
+                    } else {
+                        taxableAmountUSD = marketValuePerShare * quantity
+                    }
+                    const taxableAmountPLN = taxableAmountUSD * exchangeRate
+                    const taxAmountPLN = taxableAmountPLN * 0.19
+                    return {purchaseDate, bookValuePerShare, marketValuePerShare, quantity, taxableAmountUSD, exchangeRate, taxableAmountPLN, taxAmountPLN}
+                })
+            })
 
-	const moveOneDayBack = date => {
-		date.setDate(date.getDate() - 1)
-		return date
-	}
-
-	const fetchExchangeRate = date =>
-		fetch(`https://api.nbp.pl/api/exchangerates/rates/a/usd/${getCurrencyDateString(date)}/?format=json`)
-			.then(response => {
-				if (response.status === 200) {
-					return response.json()
-						.then(data => data.rates[0].mid)
-				}
-				if (response.status === 404) {
-					return undefined;
-				}
-				return Promise.reject(Error('problem fetching rate from nbp.pl'));
-			})
-
-
-	const getExchangeRate = (date, tries = 7) => {
-		if (tries > 0) {
-			return fetchExchangeRate(moveOneDayBack(date))
-				.then(rate => rate || getExchangeRate(date, tries - 1))
-		}
-		return Promise.reject(Error('no rate found!'))
-	}
-
-	const filterResponseGarbage = response => response.substr(response.indexOf('{'))
-
-	const getXsrfToken = () =>
-		document.cookie
-			.split(';')
-			.filter((cookie) => cookie.indexOf('XSRF-TOKEN') !== -1)
-			.pop()
-			.split('=')
-			.pop()
-
-	const processReleases = releases =>
-		Promise.all(releases
-			.map(release =>
-				Promise.all([
-					getExchangeRate(new Date(release.tradeDate)),
-					getReleaseDetails(release)
-				])
-				.then(([exchangeRate, {acquiredPrice}]) => {
-					const shareUnits = Number(release.shares)
-					const shareValue = Number(parseDollars(acquiredPrice))
-					const totalUSD = shareUnits * shareValue
-
-					return {
-						tradeDate: release.tradeDate,
-						shareUnits,
-						shareValue,
-						totalUSD,
-						exchangeRate,
-						totalPLN: totalUSD * exchangeRate
-					}
-
-				})
-			)
-		)
-		.then(partials => {
-			const totalReleaseEarned = partials
-				.reduce((previous, current) => previous + current.totalPLN, 0)
-
-			if (console.table) {
-				console.log('%crelease details:', 'color: blue')
-				console.table(partials)
-			}
-			console.log(`%creleases total is %c${totalReleaseEarned.toFixed(2)} PLN`, 'color: blue', 'color: red')
-			console.log('%cyou may want to put that into PIT/ZG p. 24 and PIT-36 p. 85 (or p. 136 for spouse)', 'color: blue')
-		})
-
-	const getDividendDetails = dividend => {
-		const orderDate = getURLDateStringYDM(new Date(dividend.orderDate))
-		return fetchData(`${JSON_BASE}/transaction/details/${COMPANY_ID}/${PLAN_ID}/K?orderDate=${orderDate}&orderNumber=${dividend.orderNumberForUIRef}&segmentId=${dividend.segmentId}&fromOrder=closedOrder&format=json`)
-			.then(data => data.orderDetail.dataSet.DATA.pop())
-	}
-
-	const getReleaseDetails = release => {
-		const orderDate = getURLDateStringYMD(new Date(release.orderDate))
-		return fetchData(`${JSON_BASE}/transaction/details/${COMPANY_ID}/${PLAN_ID}/K?orderDate=${orderDate}&orderNumber=${release.orderNumberForUIRef}&segmentId=${release.segmentId}&fromOrder=closedOrder&format=json`)
-		     .then(data => data.shareDetails.dataSet.DATA.pop())
-	}
-
-	const processDividends = dividends =>
-		Promise.all(dividends
-			.map(dividend =>
-				Promise.all([getExchangeRate(new Date(dividend.tradeDate)), getDividendDetails(dividend)])
-					.then(([exchangeRate, dividendDetails]) => {
-						const grossDividendCredit = Number(parseDollars(dividendDetails.grossDividendCredi))
-						return {
-							tradeDate: dividend.tradeDate,
-							grossDividendCredit,
-							exchangeRate,
-							totalPLN: grossDividendCredit * exchangeRate
-						}
-					})
-			)
-		)
-		.then(partials => {
-			const totalDividendEarned = partials
-				.reduce((previous, current) => previous + current.totalPLN, 0)
-
-			if (console.table) {
-				console.log('%cdividend details:', 'color: brown')
-				console.table(partials)
-			}
-			console.log(`%c19% of dividends total is %c${(totalDividendEarned * 0.19).toFixed(2)} PLN`, 'color: brown', 'color: red')
-			console.log('%cyou may want to put that into PIT-38 p. 35. Alternatively PIT-36 p. 218, if you don\'t need PIT-38', 'color: brown')
-		})
-
-	const processWithholdings = withholdings =>
-		Promise.all(withholdings
-			.map(withholding =>
-				getExchangeRate(new Date(withholding.tradeDate))
-					.then(exchangeRate => {
-						const netCashProceeds = Number(parseDollars(withholding.netCashProceeds))
-						return {
-							tradeDate: withholding.tradeDate,
-							netCashProceeds,
-							exchangeRate,
-							totalPLN: netCashProceeds * exchangeRate
-						}
-					})
-
-			)
-		)
-		.then(partials => {
-			const totalWithholdings = partials
-				.reduce((previous, current) => previous + current.totalPLN, 0)
-
-			if (console.table) {
-				console.log('%cdividend withholding details:', 'color: darkgreen')
-				console.table(partials)
-			}
-			console.log(`%cdividend witholdings total is %c${totalWithholdings.toFixed(2)} PLN`, 'color: darkgreen', 'color: red')
-			console.log('%cyou may want to put that into PIT-38 p. 36. Alternatively PIT-36 p. 220, if you don\'t need PIT-38', 'color: darkgreen')
-		})
-
-	const getSaleRelatedBuyCosts = shareLots =>
-		Promise.all(shareLots
-			.map(shareLot => getExchangeRate(new Date(shareLot.acquiredDate))
-				.then(exchangeRate => Number(shareLot.sharesSold) * Number(parseDollars(shareLot.acquiredPrice)) * exchangeRate))
-		)
-		.then(shareLotCosts => shareLotCosts.reduce((previous, current) => previous + current, 0))
-
-	const getSaleDetails = sale => {
-		const orderDate = getURLDateStringYDM(new Date(sale.orderDate))
-		return fetchData(`${JSON_BASE}/transaction/details/${COMPANY_ID}/${PLAN_ID}/K?orderDate=${orderDate}&orderNumber=${sale.orderNumber}&segmentId=${sale.segmentId}&fromOrder=closedOrder&format=json`)
-			.then(data => {
-				const proceedDetails = data.proceedsDetails.dataSet.DATA.pop()
-				const orderDetail = data.orderDetail.dataSet.DATA.pop()
-				const buyTransactions = data.shareLots.dataSet.DATA
-				return Promise.all([getExchangeRate(new Date(orderDetail.trxTradeDate)), getSaleRelatedBuyCosts(buyTransactions)])
-					.then(([exchangeRate, buyCosts]) => {
-						const sellValue = Number(parseDollars(proceedDetails.grossProceeds)) * exchangeRate
-						const transactionCost = Number(parseDollars(proceedDetails.totalFees)) * exchangeRate
-						return {
-							tradeDate: orderDetail.trxTradeDate,
-							sellValue,
-							exchangeRate,
-							transactionCost,
-							buyCosts,
-							totalCost: buyCosts + transactionCost,
-							profit: sellValue - buyCosts - transactionCost,
-							buyTransactions
-						}
-					})
-			})
-	}
-
-	const getSaleBuyTransactions = sale =>
-		Promise.all(sale.buyTransactions
-			.map(transaction =>
-				getExchangeRate(new Date(transaction.acquiredDate))
-					.then(exchangeRate => {
-						const acquiredPrice = Number(parseDollars(transaction.acquiredPrice))
-						const sharesSold = Number(transaction.sharesSold)
-						return {
-							tradeDate: transaction.acquiredDate,
-							sharesSold,
-							acquiredPrice,
-							exchangeRate,
-							acquiredPLN: sharesSold * acquiredPrice * exchangeRate
-						}
-					})
-				))
-
-	const processSales = sales => {
-		if (!sales.length) {
-			console.log('%cno sale records found', 'color: green')
-			return Promise.resolve()
-		}
-		return Promise.all(sales.map(getSaleDetails))
-			.then(sales => {
-				if (console.table) {
-					console.log('%csale details:', 'color: darkorange')
-					console.table(sales, ['tradeDate','sellValue','exchangeRate','transactionCost','buyCosts','totalCost','profit'])
-					console.log('%csale details breakdowns:', 'color: darkorange')
-					return Promise.all(sales.map((sale, index) =>
-						getSaleBuyTransactions(sale)
-							.then(buyTransactions => {
-								console.log(`%csale ${index + 1} related buy transactions`, 'color: darkorange')
-								console.table(buyTransactions)
-							}))
-						)
-						.then(() => sales)
-				}
-				return sales;
-			})
-			.then(sales => {
-				const totalSellValue = sales.reduce((previous, current) => previous + current.sellValue, 0)
-				const totalCost = sales.reduce((previous, current) => previous + current.totalCost, 0)
-				const totalProfit = sales.reduce((previous, current) => previous + current.profit, 0)
-
-				console.log('%csells grand totals:', 'color: darkorange')
-				console.log(`%csells total value is %c${totalSellValue.toFixed(2)} PLN`, 'color: darkorange', 'color: red')
-				console.log('%cyou may want to put that into PIT-38 p. 22', 'color: darkorange')
-				console.log(`%csells total cost is %c${totalCost.toFixed(2)} PLN`, 'color: darkorange', 'color: red')
-				console.log('%cyou may want to put that into PIT-38 p. 23', 'color: darkorange')
-				console.log(`%csells total profit is %c${totalProfit.toFixed(2)} PLN`, 'color: darkorange', 'color: red')
-				console.log('%cyou may want to put that into PIT/ZG p. 31', 'color: darkorange')
-			})
-	}
-
-	console.info('%cfetching data...', 'font-weight: bold;')
-	console.warn('%cplease ignore the \"%cGET https://api.nbp.pl/api/exchangerates/rates/a/usd/YYYY-MM-DD/?format=json 404 (Not Found - Brak danych)\" errors logged on the console by the browser - %cTHEY ARE EXPECTED', 'font-weight: bold;', 'font-style: italic', 'color:green;font-weight: bold;')
-
-	fetchData(`${JSON_BASE}/transactions/${COMPANY_ID}/K/${PLAN_ID}/duration/between/${PERIOD_START}/${PERIOD_END}?format=json`)
-		.then(data => {
-			const records = data.closedOrders.dataSet.DATA
-			if (records && records.length) {
-				return Promise.all([
-					processReleases(records.filter((record) => record.transactionType === 'Released Shares')),
-					processDividends(records.filter((record) => record.transactionType === 'Dividend Credit')),
-					processWithholdings(records.filter((record) => record.transactionType === 'IRS Withholding')),
-					processSales(records.filter((record) => record.transactionType === 'Sale'))
-				])
-			} else {
-				console.warn(`seems like there is no activity in your account for the period from ${PERIOD_START} to ${PERIOD_END}`)
-			}
-		})
-		.then(() => {
-			console.log('%call done ~ FIN ~', 'color: green')
-		})
-		.catch(error => console.log('ups!, ' + error))
+    fetchHistoryRecords()
+        .then(records => records.filter(record => record.txApiType.includes('WITHDRAWAL') && (new Date(record.settlementDate)).getFullYear() === theYear))
+        .then(sales => Promise.all(sales.map(sale => {
+            const promise = sale.txApiType === 'WITHDRAWAL_REALTIME_TRANSACTION' ? fetchRtSaleDetails(sale.realtimeTransactionPK) : fetchSaleDetails(sale.spfWithdrawalPK)
+            return promise.then(processSaleDetails)
+                .then(taxableDetails => ({
+                    transactionDate: sale.transactionDate,
+                    settlementDate: sale.settlementDate,
+                    quantity: sale.quantity,
+                    taxableDetails
+                }))
+        })))
+        .then(sales => {
+            if (sales.length) {
+                let grandTaxTotalPLN = 0;
+                sales.forEach(sale => {
+                    const totalTaxAmountPLN = sale.taxableDetails.reduce((acc, curr) => acc + curr.taxAmountPLN , 0)
+                    grandTaxTotalPLN += totalTaxAmountPLN
+                    console.log(`You sold ${sale.quantity} shares on ${sale.transactionDate} (settled on ${sale.settlementDate}). Total tax amount is ${totalTaxAmountPLN.toFixed(2)} PLN. Details:`)
+                    console.table(sale.taxableDetails)
+                })
+                console.log(`all of that tax sums up to ${grandTaxTotalPLN.toFixed(2)} PLN`)
+            } else {
+                console.log(`seems like there is nothing to declare (no sales in ${theYear}), but please double check just in case`)
+            }
+        })
 })()
